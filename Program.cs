6e8f9f89dd48fe8556474efff2d65bafbed883f2
berre6 <berreyilmaz666@gmail.com>
2025-10-03 16:08:46 +0000

    Çalışan kodu kaydet

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan iki sayı al
+        Console.Write("Birinci sayıyı girin: ");
+        int sayi1 = int.Parse(Console.ReadLine());
+
+        Console.Write("İkinci sayıyı girin: ");
+        int sayi2 = int.Parse(Console.ReadLine());
+
+        // Büyük olan sayıyı bul
+        int buyukSayi = (sayi1 > sayi2) ? sayi1 : sayi2;
+
+        // Tek mi çift mi kontrol et
+        if (buyukSayi % 2 == 0)
+        {
+            Console.WriteLine("Büyük sayı {0} ve çift.", buyukSayi);
+        }
+        else
+        {
+            Console.WriteLine("Büyük sayı {0} ve tek.", buyukSayi);
+        }
+    }
+}
+
